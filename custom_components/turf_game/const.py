8f1a63b1c4bf3bbc33adc760865506fcccb23464
Johannes Hansson <johannes@ajjm.se>
2026-05-23 18:30:32 +0200

    Refactor integration name from Hello World to Turf Game in config flow and constants

--- custom_components/turf_game/const.py
+++ custom_components/turf_game/const.py
@@ -1,5 +1,2 @@
-"""Constants for the Detailed Hello World Push integration."""
-
-# This is the internal name of the integration, it should also match the directory
-# name for the integration.
+"""Constants for the Turf Game integration."""
 DOMAIN = "turf_game"
